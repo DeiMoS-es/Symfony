b34b844c400546b1fe9eb93bef571ade19946aa8
DeiMoS <nagib.86@gmail.com>
2026-01-04 08:39:25 +0000

    Vercel

--- club-cine/api/index.php
+++ club-cine/api/index.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Kernel;
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', false);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
