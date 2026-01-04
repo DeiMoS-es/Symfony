13fdd7ff72bc99b9a4e14ab99e905880934cea33
DeiMoS <nagib.86@gmail.com>
2026-01-04 13:01:48 +0000

    Ultimo ajuste

--- club-cine/public/index.php
+++ club-cine/public/index.php
@@ -1,8 +1,20 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Configuración para que Symfony confíe en el proxy de Vercel
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+    Request::setTrustedProxies(
+        ['127.0.0.1', 'REMOTE_ADDR'],
+        Request::HEADER_X_FORWARDED_FOR | 
+        Request::HEADER_X_FORWARDED_HOST | 
+        Request::HEADER_X_FORWARDED_PROTO | 
+        Request::HEADER_X_FORWARDED_PORT
+    );
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
