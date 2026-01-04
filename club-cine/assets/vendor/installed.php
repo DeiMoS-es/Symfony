984a699ab11235f470bb1a4cc4b27cb9de577b12
DeiMoS <nagib.86@gmail.com>
2026-01-04 10:37:56 +0000

    Build de producción para Vercel

---
+++ club-cine/assets/vendor/installed.php
@@ -0,0 +1,32 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'flash-messages' => 
+  array (
+    'version' => '0.0.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
