cb698c23eb819cc901bf22a1a99cdd43348e06a0
Nagib <nagib.86@gmail.com>
2026-01-18 11:48:54 +0100

    Implementada búsqueda por título en dashboard, actualizado manejo de solicitudes botones de paginación

--- club-cine/src/Module/Movie/Controller/DashboardController.php
+++ club-cine/src/Module/Movie/Controller/DashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Module\Movie\Controller;
 
-use App\Module\Movie\Service\TmdbService;
+use App\Module\Movie\Service\MovieService; // <--- Cambiamos el service
 use App\Module\Auth\Entity\User;
 use App\Module\Auth\Mapper\UserMapper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,23 +13,29 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/movies')]
 class DashboardController extends AbstractController
 {
+
     #[Route('/dashboard', name: 'user_dashboard', methods: ['GET'])]
-    public function dashboard(Request $request, TmdbService $tmdbService): Response
+    public function dashboard(Request $request, MovieService $movieService): Response
     {
         /** @var User|null $user */
         $user = $this->getUser();
 
-        // 1. Convertimos la Entidad en un DTO limpio usando tu Mapper
-        // Ahora $userDto tiene .email, .name, .groupId y .groupName
         $userDto = $user ? UserMapper::toResponseDTO($user) : null;
 
+        // 1. Capturamos el término de búsqueda
+        $searchTerm = $request->query->get('q', '');
+        
         $page = $request->query->getInt('page', 1);
-        $catalog = $tmdbService->fetchPopularCatalog($page);
+
+        // 2. Usamos el nuevo método que creamos y testeamos
+        $catalog = $movieService->getSearchCatalog($searchTerm, $page);
+
         return $this->render('dashboard.html.twig', [
-            'movies' => $catalog,
-            'user'   => $userDto, 
-            'totalPages' => $catalog['total_pages'] ?? 1, 
+            'movies'      => $catalog,
+            'user'        => $userDto, 
+            'totalPages'  => $catalog['total_pages'] ?? 1, 
             'currentPage' => $page,
+            'searchTerm'  => $searchTerm, // 3. Lo pasamos a la vista
         ]);
     }
 }
