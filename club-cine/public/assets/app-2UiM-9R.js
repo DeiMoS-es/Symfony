3acb7e64699a03769937fe25e24aad278302a7eb
DeiMoS <nagib.86@gmail.com>
2026-02-14 11:44:53 +0100

    Subir carpeta public/assets

---
+++ club-cine/public/assets/app-2UiM-9R.js
@@ -0,0 +1,10 @@
+import './stimulus_bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+// CSS is now loaded directly via <link> tag in base.html.twig for better performance
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
